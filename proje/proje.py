2cd290db3a3ec9ebbb9405db37e96c2fea921b4b
Vilibonka <112898583+Vilibonka@users.noreply.github.com>
2025-03-05 04:36:52 +0300

    Add files via upload

--- proje/proje.py
+++ proje/proje.py
@@ -30,22 +30,34 @@ def get_current_weather(city_name):
         return f"Hata: Şehir bulunamadı ({city_name})"
     
     weather_description = data["weather"][0]["description"]
-    
     temperature = data["main"]["temp"]
     feels_like = data["main"]["feels_like"]
     humidity = data["main"]["humidity"]
     wind_speed = data["wind"]["speed"]
+    pressure = data["main"]["pressure"]
+    sunrise = datetime.fromtimestamp(data["sys"]["sunrise"]).strftime('%H:%M:%S')
+    sunset = datetime.fromtimestamp(data["sys"]["sunset"]).strftime('%H:%M:%S')
     icon_code = data["weather"][0]["icon"]
     icon_url = f"http://openweathermap.org/img/wn/{icon_code}@2x.png"
+    
+    alerts= data.get("alerts",[])
+    alert_messages =[]
+    
+    for alert in alerts:
+        alert_messages.append(f"{alert['event']}:{alert['description']}")
 
     result = {
         "city":city_name,
         "weather":weather_description.capitalize(),
         "temperature":f"{temperature}°C",
         "feels_like":f"{feels_like}°C",
-        "humidity":f"{humidity}",
+        "humidity":f"{humidity}%",
         "wind_speed":f"{wind_speed} m/s",
-        "icon_url":icon_url        
+        "pressure":f"{pressure} hPa",
+        "sunrise":sunrise,
+        "sunset":sunset,
+        "icon_url":icon_url,
+        "alerts":alert_messages 
         }
     
     return result
@@ -82,19 +94,74 @@ def get_weather_forecast(city_name):
     
     return forecast_result
 
+@app.route("/weather/location",methods=["POST"])
+def weather_by_location():
+    
+    latitude = request.json.get("latitude")
+    longitude = request.json.get("longitude")
+    
+    if not latitude or not longitude:
+        return {"error":"Konum bilgisi alınamadı"},400
+    
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
+    
+    params = {
+        "lat":latitude,
+        "lon":longitude,
+        "appid":api_key,
+        "units":"metric",
+        "lang":"tr"
+    }
+    
+    response = requests.get(base_url, params=params)
+    data = response.json()
+    
+    if response.status_code != 200:
+        return {"error":"Hava durumu bilgisi alınamadı!"},400
+    
+    sunrise = datetime.fromtimestamp(data["sys"]["sunrise"]).strftime('%H:%M:%S')
+    sunset = datetime.fromtimestamp(data["sys"]["sunset"]).strftime('%H:%M:%S')
+    
+    weather = {
+        "city": data["name"],
+        "weather": data["weather"][0]["description"].capitalize(),
+        "temperature": f"{data['main']['temp']}°C",
+        "feels_like": f"{data['main']['feels_like']}°C",
+        "humidity": f"{data['main']['humidity']}%",
+        "wind_speed": f"{data['wind']['speed']} m/s",
+        "pressure": f"{data['main']['pressure']} hPa",
+        "sunrise": sunrise,
+        "sunset": sunset,
+        "icon_url": f"http://openweathermap.org/img/wn/{data['weather'][0]['icon']}@2x.png"
+    }
+    return weather
+
+@app.route("/location-weather")
+def location_weather():
+    return render_template("location_weather.html")
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/weather", methods=["POST"])
+@app.route("/weather", methods=["GET","POST"])
 def weather():
-    cities_input = request.form.get("cities")
+    if request.method == "POST":
+        cities_input = request.form.get("cities")
     
-    if not cities_input:
-        return "Hata: Şehir adı boş olamaz!"
+        if not cities_input:
+            return "Hata: Şehir adı boş olamaz!"
     
-    city_names = [city.strip() for city in cities_input.split(",")]
+        city_names = [city.strip() for city in cities_input.split(",")]
     
+    elif request.method == "GET":
+        cities_input = request.args.get("cities")
+        
+        if not cities_input:
+            return "Hata: Şehir adı boş olamaz!"
+        
+        city_names = [city.strip() for city in cities_input.split(",")]
+        
     weather_data_list = []
     
     for city in city_names:
